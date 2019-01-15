4be54c6a8d6602e3f5e088c70d8427edf6088722
lead1974 <email2lead@gmail.com>
2019-01-14 16:54:04 -0800

    AP 01.14.19 Start of Admin screens

---
+++ DMCore/Areas/Admin/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DMCore.Data.Core;
+using DMCore.Data.Core.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DMCore.Areas.Admin.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public List<DealCategory> dcList { get; set; } = new List<DealCategory>();
+        public void OnGet()
+        {
+
+            dcList = _unitOfWork.DealCategories.GetAll().ToList();
+        }
+    }
+}
